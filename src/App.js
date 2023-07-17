0b132258c082085789d274b0bffd49a570efdb8b
xz-1 <skonchan199w@gmail.com>
2023-07-17 10:54:42 -0400

    Second Commit: Finished Redux state manangment code in /state/index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+//https://youtu.be/K8YELRmUb5o?t=6752 : the reason to use jsconfig.json
+//normally import HomePage from "./scenes/homePage";
+//to just: note the './' is omitted because in the 'jsconfig.json' is set at 'src' folder
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+
 
 function App() {
+
+
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/home" element={<HomePage />}></Route>
+          <Route path="/profile/:userId" element={<ProfilePage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/state/index.js
@@ -0,0 +1,64 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+//this will be the state that is store in the Global state
+//so, it will be accessable throughtout the entire application
+const initialState = {
+    mode: "light",
+    user: null,
+    token: null,
+    posts: [],
+};
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    //this action/fucntion:
+    //the modify the Global state in the initialState code above
+    reducers: {
+        setMode: (state) => {
+            //this will take cake of switching mode if it's LIGHT switch it to DARK
+            //if it's DARK switch it to LIGHT
+            //Note: this 'state.mode' reference to the current 'mode: "light" line within initialSate above'
+            state.mode = state.mode === "light" ? "dark" : "light";
+        },
+        setLogin: (state, action) => {
+            //this the info from 'user' parameter from 'action' parameter/payload
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        },
+        //when the user hit the 'Log out Button': 'state' will be reset to have nothing in there
+        setLogOut: (state) => {
+            state.user = null;
+            state.token = null;
+        },
+        setFriends: (state, action) => {
+            if (state.user) {
+                state.user.friends = action.payload.friends;
+            } else {
+                console.error("User Friend Non-Existent");
+            }
+        },
+        setPosts: (state, action) => {
+            state.posts = action.payload.posts;
+        },
+        //this is most complicated one
+        setPost: (state, action) => {
+            //grasping a list of post and map through each one
+            const updatedPosts = state.posts.map((post) => {
+                //                  current post that is sent thorugh this function
+                if (post._id === action.payload.post_id) return action.payload.post;
+                return post;
+            });
+            state.post = updatedPosts;
+        },
+    }
+});
+
+
+
+
+//this is the part of Redux toolkits
+export const { setMode, setLogin, setLogOut, setFriends, setPosts, setPost } = authSlice.actions;
+export default authSlice.reducer;
+
+//Note: this will be the entire "State"
